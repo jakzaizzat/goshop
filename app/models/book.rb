fbc63c52becd279041b8a7219164fe76f18d8b45
jakzaizzat <jakzaizzat@gmail.com>
2016-03-27 09:48:55 +0800

    Added sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,9 +5,14 @@ class Book < ActiveRecord::Base
 
 
 	belongs_to :user
+	has_many :sales
 
 	has_attached_file :image
 	has_attached_file :resource
+
+
+	validates_numericality_of :price, 
+	greater_than: 49, message: "Must be at least 50 cents"
 
 	validates_attachment_content_type :image, 
 	content_type: /^image\/(png|gif|jpeg)/,
---
+++ app/models/sale.rb
@@ -0,0 +1,13 @@
+class Sale < ActiveRecord::Base
+
+	before_create :populate_guid
+
+	belongs_to :book
+
+	private
+
+	def populate_guid
+		self.guid = SecureRandom.uuid()
+	end
+
+end
